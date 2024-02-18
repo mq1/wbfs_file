e127bb54b00f792f2c1cbb100e7ee0570c7b197c
Manuel Quarneti <manuelquarneti@protonmail.com>
2024-02-18 10:08:35 +0100

    👷 Update macos ci

--- build-macos.sh
+++ build-macos.sh
@@ -13,6 +13,25 @@ clang \
     -I source -I source/libwbfs \
     -DLARGE_FILES -D_FILE_OFFSET_BITS=64 \
     -Oz \
-    -o wbfs_file
+    -arch x86_64 \
+    -o wbfs_file-x86_64
+strip wbfs_file-x86_64
 
-strip wbfs_file
+clang \
+    source/wbfs.c \
+    source/tools.c \
+    source/bn.c \
+    source/ec.c \
+    source/libwbfs/libwbfs.c \
+    source/libwbfs/wiidisc.c \
+    source/libwbfs/rijndael.c \
+    source/splits.c \
+    source/libwbfs/libwbfs_osx.c \
+    -I source -I source/libwbfs \
+    -DLARGE_FILES -D_FILE_OFFSET_BITS=64 \
+    -Oz \
+    -arch arm64 \
+    -o wbfs_file-arm64
+strip wbfs_file-arm64
+
+lipo wbfs_file-x86_64 wbfs_file-arm64 -create -output wbfs_file-Universal2
